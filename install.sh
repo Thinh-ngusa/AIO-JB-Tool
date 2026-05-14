0851e0f65b0553a5aa39773d2d9ad8804d1dae11
Sinszxmc <trthnh2002@gmail.com>
2026-05-14 15:21:57 +0700

    Add install script

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+clear
+
+echo "[*] Installing AIO JB Tool..."
+
+echo
+echo "[*] Cloning repository..."
+
+git clone https://github.com/Thinh-ngusa/AIO-JB-Tool.git
+
+cd AIO-JB-Tool || exit
+
+echo
+echo "[*] Installing Python dependencies..."
+
+python3 -m pip install colorama
+
+echo
+echo "[*] Setting executable permissions..."
+
+chmod +x resources/scripts/palehide-beta7/palehide.sh
+chmod +x resources/scripts/palehide-beta7/palera1n-macos-universal
+chmod +x resources/tools/trollrestore-amd64
+chmod +x resources/tools/trollrestore-arm64
+
+echo
+echo "[*] Creating global launcher..."
+
+sudo tee /usr/local/bin/aiojb > /dev/null <<EOF
+#!/bin/bash
+cd "$(pwd)" || exit
+python3 main.py
+EOF
+
+sudo chmod +x /usr/local/bin/aiojb
+
+echo
+echo "[+] Installation complete."
+echo "[+] Run the tool anytime using:"
+echo
+echo "    aiojb"
+echo
